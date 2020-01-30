c2cf7b7435a6031e4c5e09a59cf6a16dd78bb2fc
Seung Hyun Soun <jamessoun93@gmail.com>
2020-01-30 15:25:57 +0900

    [웹팩 및 바벨 환경설정]
    - 리액트에서 바벨을 사용하기 위한 프리셋 추가
    - 엔트리 포인트 및 빌드버전 파일경로 지정

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: './index.html'
+    })
+  ]
+};
