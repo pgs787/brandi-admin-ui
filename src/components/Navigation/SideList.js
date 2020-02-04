4b986d140d0a0d34be465eb6160a071857aa4251
yoosg <yousk6347@gmail.com>
2020-02-04 19:44:37 +0900

    [네비게이션]  - API 연동전 목업데이터로 모든 리스트 렌더링.  - SideBar fold기능 추가.  - Footer 추가.  - API연동 & 사용자 권한에 따른 menulist 렌더링 완료.

--- src/components/Navigation/SideList.js
+++ src/components/Navigation/SideList.js
@@ -1,177 +1,149 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled, { css } from 'styled-components';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
-import TreeView from '@material-ui/lab/TreeView';
-import TreeItem from '@material-ui/lab/TreeItem';
-import Typography from '@material-ui/core/Typography';
 import RemoveIcon from '@material-ui/icons/Remove';
-import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
-import ArrowRightIcon from '@material-ui/icons/ArrowRight';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Collapse from '@material-ui/core/Collapse';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
 import data from './Data';
 
-const useTreeItemStyles = makeStyles(theme => ({
+const useStyles = makeStyles(theme => ({
   root: {
-    color: theme.palette.text.secondary,
-    '&:focus > $content': {
-      backgroundColor: `var(--tree-view-bg-color, ${theme.palette.grey[400]})`,
-      color: 'var(--tree-view-color)',
-    },
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
   },
-  hide: {
-    display: 'none',
+  menu: {
+    paddingTop: '0px',
+    paddingBottom: '0px',
   },
-  show: {
-    display: 'block',
-  },
-  content: {
-    color: theme.palette.text.secondary,
-    borderTopRightRadius: theme.spacing(2),
-    borderBottomRightRadius: theme.spacing(2),
-    padding: '4px 0px',
-    paddingRight: theme.spacing(1),
-    fontWeight: theme.typography.fontWeightMedium,
-    '$expanded > &': {
-      fontWeight: theme.typography.fontWeightRegular,
-    },
-  },
-  group: {
-    marginLeft: 0,
-    '& $content': {
-      paddingLeft: theme.spacing(2),
-    },
-  },
-  expanded: {
+  expand: {
     color: '#999',
   },
-  label: {
-    fontWeight: 'inherit',
-    color: "'inherit'",
+  nested: {
+    paddingLeft: '20px',
   },
-  labelRoot: {
-    display: 'flex',
-    alignItems: 'center',
-    padding: theme.spacing(0.5, 0),
-  },
-  labelIcon: {
-    marginRight: theme.spacing(1),
-    color: '#999',
-    fontSize: '20px',
-  },
-  labelText: {
-    fontWeight: 'inherit',
-    color: '#999',
-    flexGrow: 1,
-  },
-  arrow: {
+  topIcon: {
+    minWidth: '30px',
     color: '#999',
   },
-  item: {
-    marginTop: '20px',
-    marginBottom: '20px',
+  icon: {
+    minWidth: '20px',
+    color: '#999',
   },
-  link: {
-    textDecoration: 'none',
+  text: {
+    letterSpacing: '-1px',
+  },
+  remove: {
+    fontSize: 'small',
   },
 }));
 
-function StyledTreeItem(props) {
-  const classes = useTreeItemStyles();
-  const {
-    labelText,
-    labelIcon: LabelIcon,
-    labelInfo,
-    color,
-    bgColor,
-    ...other
-  } = props;
-
-  return (
-    <TreeItem
-      label={
-        <div className={classes.labelRoot}>
-          <LabelIcon color="inherit" className={classes.labelIcon} />
-          <Typography variant="body2" className={classes.labelText}>
-            {labelText}
-          </Typography>
-          <Typography variant="caption" color="inherit">
-            {labelInfo}
-          </Typography>
-        </div>
-      }
-      style={{
-        '--tree-view-color': color,
-        '--tree-view-bg-color': bgColor,
-      }}
-      classes={{
-        root: classes.root,
-        content: classes.content,
-        expanded: classes.expanded,
-        group: classes.group,
-        label: classes.label,
-      }}
-      {...other}
-    />
-  );
-}
-
-StyledTreeItem.propTypes = {
-  bgColor: PropTypes.string,
-  color: PropTypes.string,
-  labelIcon: PropTypes.elementType.isRequired,
-  labelInfo: PropTypes.string,
-  labelText: PropTypes.string.isRequired,
-};
-
-const useStyles = makeStyles({
-  root: {
-    height: 264,
-    flexGrow: 1,
-    maxWidth: 400,
-  },
-});
-
-const SideList = ({ open }) => {
+const SideList = ({ open, menuData }) => {
+  const [openID, setOpenID] = useState(data.foldData);
   const classes = useStyles();
+
+  // menulist fold
+  const handleClick = id => {
+    let set = [
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+    ];
+    if (openID[id] === true) {
+      setOpenID(set);
+    } else {
+      set[id] = true;
+      setOpenID(set);
+    }
+  };
+
   return (
     <Hide open={!open}>
-      <TreeView
-        className={classes.root}
-        defaultExpanded={['3']}
-        defaultCollapseIcon={<ArrowDropDownIcon />}
-        defaultExpandIcon={<ArrowRightIcon className={classes.arrow} />}
-        defaultEndIcon={<div style={{ width: 24 }} />}
-      >
-        {data.sideList.map(item => (
-          <StyledTreeItem
-            key={item.id}
-            nodeId={item.id}
-            labelText={item.name}
-            labelIcon={item.icon}
-            bgColor="#E6F4EA"
-            className={classes.item}
+      {menuData.map((item, index) => (
+        <List key={index} className={classes.menu}>
+          <ListItem
+            button
+            onClick={() => {
+              handleClick(index);
+            }}
           >
+            <ListItemIcon className={classes.topIcon}>
+              {data.listData[item.topmenu][1]}
+            </ListItemIcon>
+            <ListItemText
+              primary={
+                <div
+                  style={{
+                    letterSpacing: '-1px',
+                    fontSize: '16px',
+                    color: '#999',
+                  }}
+                >
+                  {item.topmenu}
+                </div>
+              }
+            />
+            {openID[index] ? (
+              <ExpandLess className={classes.expand} />
+            ) : (
+              <ExpandMore className={classes.expand} />
+            )}
+          </ListItem>
+          <Collapse in={openID[index]} timeout="auto" unmountOnExit>
             {item.list.map(listItem => (
               <Link
-                to={listItem.link}
+                to={listItem.url}
                 key={listItem.id}
                 className={classes.link}
               >
-                <StyledTreeItem
-                  nodeId={listItem.id}
-                  labelText={listItem.name}
-                  labelIcon={RemoveIcon}
-                  bgColor="#E6F4EA"
-                />
+                <List component="div" disablePadding>
+                  <ListItem button className={classes.nested}>
+                    <ListItemIcon className={classes.icon}>
+                      <RemoveIcon className={classes.remove} />
+                    </ListItemIcon>
+                    <ListItemText
+                      primary={
+                        <div
+                          style={{
+                            letterSpacing: '-1px',
+                            fontSize: '13.5px',
+                            color: '#999',
+                          }}
+                        >
+                          {listItem.name}
+                        </div>
+                      }
+                      className={classes.text}
+                    />
+                  </ListItem>
+                </List>
               </Link>
             ))}
-          </StyledTreeItem>
-        ))}
-      </TreeView>
+          </Collapse>
+        </List>
+      ))}
     </Hide>
   );
 };
+
 const Hide = styled.div`
   ${props =>
     props.open &&
